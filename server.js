bfaffe32c99337db7ec555d799dd8f6905247fa4
Siobhan Willoughby <shibbles@SuperMac3000.local>
2019-04-28 17:18:59 +0100

    Fixing server bleps

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/shibblesio'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -13,5 +13,5 @@ app.listen(process.env.PORT || 8080);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/shibblesio/index.html'));
 });
